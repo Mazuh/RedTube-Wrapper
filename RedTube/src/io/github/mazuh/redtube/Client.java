48fb4624e7e8abab5305388e6cd3076a83711824
Mazuh <marcell-mz@hotmail.com>
2017-05-01 18:08:23 -0300

    Simplify client and start manual tests

--- RedTube/src/io/github/mazuh/redtube/Client.java
+++ RedTube/src/io/github/mazuh/redtube/Client.java
@@ -1,15 +1,10 @@
 package io.github.mazuh.redtube;
 
-import io.github.mazuh.redtube.exceptions.MalformedURLError;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.HashMap;
 
 /** A HTTP client for simple access RedTube API.
  * Made to be used by some wrapper.
@@ -19,37 +14,45 @@ import java.io.InputStreamReader;
 public class Client {
     
     
-    private final static String ROOT_ENDPOINT = "https://api.redtube.com/";
+    //private final static String ENDPOINT = "https://api.redtube.com/";
+    private final static String ENDPOINT = "https://api.redtube.com/?data=redtube.Videos.searchVideos&output=xml&search=hard&tags[]=Teen&thumbsize=all&";
     private final static String DEFAULT_METHOD = "GET";
     
     /** Assemble a HTTP request, send it and return its response.
      * It's already configured to access RedTube API only.
      *
      * @param parameters    url HTTP params to attach before sending the request
+     *                      (can be null if there isn't any)
      * @return              a string of all the retrieved response
      * @throws IOException  if the sending or receiving couldn't be done.
      */
-    public static String execute(String parameters) throws IOException{
+    public static String execute(HashMap parameters) throws IOException{
         
         HttpURLConnection openedConnection = null;
         String response = "";
         int responseCode;
-
+        
         try{
             
-            openedConnection = Client.createOpenedHttpConnection();
-
-            Client.sendRequest(openedConnection, parameters);
+            URL url = Client.generateURL(parameters);
+            
+            openedConnection = (HttpURLConnection) url.openConnection();
+            openedConnection.setRequestMethod(DEFAULT_METHOD);
+            openedConnection.setUseCaches(false);
+            openedConnection.setDoOutput(true);
             
             if ((responseCode = openedConnection.getResponseCode()) == 200)
-                response = Client.readResponse(openedConnection);
+                response = openedConnection.getResponseMessage(); // print OK
             else
                 throw new IOException("Retrieved HTTP code: " + responseCode + ".");
-
-        } finally{
+            
+        } catch (MalformedURLException e){
+            throw new IOException("Failed to send HTTP request: client 400 error.", e);
+        } catch (IOException e){
+            throw new IOException("Failed to send HTTP request.", e);
+        }finally{
             if (openedConnection != null)
                 openedConnection.disconnect();
-            
         }
         
         return response;
@@ -57,69 +60,24 @@ public class Client {
     }
     
     
-    
-    private static HttpURLConnection createOpenedHttpConnection() throws IOException{
-
+    private static URL generateURL(HashMap parameters) throws MalformedURLException{
+        
+        StringBuilder url = new StringBuilder(Client.ENDPOINT);
+        
+        if (parameters != null){
+            parameters.forEach((key, value) -> {
+                url.append(key).append('=').append(value).append('&');
+            });
+        }
+        
         try{
             
-            URL url = new URL(Client.ROOT_ENDPOINT);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            connection.setRequestMethod(Client.DEFAULT_METHOD);
-
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
-
-            return connection;
-        
-        } catch(MalformedURLException e){
-            throw new MalformedURLError("Bad root URL for API.", e);
-        } catch(IOException e){
-            throw new IOException("Failed to open HTTP connection.", e);
+            return new URL(url.toString());
+            
+        } catch (MalformedURLException e){
+            throw new MalformedURLException("Bad URL while creating request: " + url.toString());
         }
         
     }
-    
-    
-    
-    private static void sendRequest(HttpURLConnection openedConnection, String parameters) throws IOException {
-        
-        openedConnection.setRequestProperty(
-                "Content-Length",
-                Integer.toString(parameters.getBytes().length)
-        );
-        
-        try (DataOutputStream output = new DataOutputStream(openedConnection.getOutputStream())) {
-            output.writeBytes(parameters);
-        } catch(IOException e){
-            throw  new IOException("Failed to send HTTP request.", e);
-        }
 
-    }
-    
-    
-    
-    private static String readResponse(HttpURLConnection openedConnection) throws IOException{
-        
-        InputStream input = openedConnection.getInputStream();
-        
-        try (BufferedReader bufferReader = new BufferedReader(new InputStreamReader(input))) {
-            
-            StringBuilder responseBuffer = new StringBuilder();
-            String line;
-            
-            while((line = bufferReader.readLine()) != null){
-                responseBuffer.append(line);
-                responseBuffer.append('\r');
-            }
-            
-            return responseBuffer.toString();
-            
-        } catch(IOException e){
-            throw new IOException("Failed to handle HTTP response.", e);
-        }
-        
-    }
-    
-    
 }
--- RedTube/src/io/github/mazuh/redtube/RedTube.java
+++ RedTube/src/io/github/mazuh/redtube/RedTube.java
@@ -1,9 +1,20 @@
 package io.github.mazuh.redtube;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mazuh
  */
 public class RedTube {
-    
+    public static void main(String[] args){
+        try {
+            String oe = Client.execute(null);
+            System.out.println(oe);
+        } catch (IOException ex) {
+            Logger.getLogger(RedTube.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
