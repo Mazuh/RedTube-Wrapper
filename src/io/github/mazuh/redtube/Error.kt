96d760bb87896c9f26234f83b077db378bef025f
Mazuh <marcell-mz@hotmail.com>
2017-05-21 07:11:12 -0300

    Init documented errors implementation (need tests)

---
+++ src/io/github/mazuh/redtube/Error.kt
@@ -0,0 +1,17 @@
+package io.github.mazuh.redtube
+
+/** Represents all documented RedTube error messages.
+ * TODO: will it be turned into exceptions classes?
+ *
+ * @author mazuh
+ */
+enum class Error(val code: Int){
+    NO_SUCH_METHOD(1001),
+    NO_SUCH_DATA_PROVIDER(1002),
+    NO_INPUT_PARAMETERS_SPECIFIED(1003),
+    NO_VIDEOS_FOUND(2001),
+    NO_VIDEO_WITH_THIS_ID(2002),
+    NO_CATEGORIES_FOUND(3001),
+    NO_TAGS_FOUND(3002),
+    NO_STARS_FOUND(3003),
+}
