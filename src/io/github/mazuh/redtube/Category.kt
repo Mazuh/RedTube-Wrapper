bf4fb4feaae08b125c036f6c6393f72329542efb
Mazuh <marcell-mz@hotmail.com>
2017-05-21 07:10:39 -0300

    Add additional domain classes

---
+++ src/io/github/mazuh/redtube/Category.kt
@@ -0,0 +1,11 @@
+package io.github.mazuh.redtube
+
+/** Represents a domain class for any RedTube category.
+ *
+ * @author mazuh
+ */
+class Category {
+    
+    val name: String? = null
+    
+}
---
+++ src/io/github/mazuh/redtube/Star.kt
@@ -0,0 +1,15 @@
+package io.github.mazuh.redtube
+
+import java.net.URL
+
+/** Represents a domain class for any RedTube porn star.
+ *
+ * @author mazuh
+ */
+class Star {
+    
+    val name: String? = null
+    val thumb: Thumb? = null
+    val url: URL? = null
+    
+}
